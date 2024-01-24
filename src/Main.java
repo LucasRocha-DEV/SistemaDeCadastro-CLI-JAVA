05a2213d6663ff8eb7e4b4e5204858fcc9fa0ef0
LucasRocha <lucaspdsr@gmail.com>
2024-01-23 21:43:50 -0300

    Implementação do Logger para tratamento robusto de exceções substituindo o uso de 'printStackTrace()'.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         String arquivo = "formulario.txt";
 
@@ -12,7 +16,7 @@ public class Main {
                 System.out.println(linha);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
         }
     }
 }
